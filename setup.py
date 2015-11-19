9805eb1643b78322f13ab4fa94f5ad6c27ae3552
Steinar Sturlaugsson <steinar.sturlaugsson@medsci.uu.se>
2015-11-19 09:53:15 +0100

    Add to package

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='confluence-tools',
+    version='0.1.0',
+    description='Tools for working with Confluence, in particular release management.',
+    long_description=long_description,
+    url='https://github.com/withrocks/release-tools',
+    author='withrocks',
+
+    license='MIT',
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='development deployment release',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['requests', 'click'],
+
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'confluence-tools=confluence_tools.cli:cli_main',
+        ],
+    },
+)
+
